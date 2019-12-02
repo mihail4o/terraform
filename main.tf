4281a59716e3e6b2336a027e20c9788eb88268a2
Mihail Mihov <mihail4o@gmail.com>
2019-12-01 20:39:03 -0800

    Azure Load Balancing implementation

--- main.tf
+++ main.tf
@@ -8,10 +8,11 @@ variable "web_server_address_space" {}
 variable "web_server_name" {}
 variable "environment" {}
 variable "web_server_count" {}
-variable "terraform_script_version" {}
 variable "web_server_subnets" {
   type = list(string)
 }
+variable "terraform_script_version" {}
+variable "domain_name_label" {}
 
 
 variable "web_server_location" {}
@@ -34,7 +35,7 @@ resource "azurerm_resource_group" "web_server_rg" {
   name     = var.web_server_rg
   location = var.web_server_location
 
-  tags {
+  tags = {
     environment   = local.build_environment
     build-version = var.terraform_script_version
   }
@@ -52,22 +53,17 @@ resource "azurerm_subnet" "web_server_subnet" {
   resource_group_name  = azurerm_resource_group.web_server_rg.name
   virtual_network_name = azurerm_virtual_network.web_server_vnet.name
   address_prefix       = var.web_server_subnets[count.index]
-  # network_security_group_id = azurerm_network_security_group.web_server_nsg.id
+  network_security_group_id = count.index == 0 ? azurerm_network_security_group.web_server_nsg.id : ""
   count                = length(var.web_server_subnets)
 
 }
 
-resource "azurerm_subnet_network_security_group_association" "web_server_subnet_sga" {
-  network_security_group_id = azurerm_network_security_group.web_server_nsg.id
-  subnet_id                 = azurerm_subnet.web_server_subnet[count.index].id
-  count                     = length(var.web_server_subnets)
-}
-
-resource "azurerm_public_ip" "web_server_public_ip" {
+resource "azurerm_public_ip" "web_server_lb_public_ip" {
   name                = "${var.resource_prefix}-public-ip"
   location            = var.web_server_location
   resource_group_name = azurerm_resource_group.web_server_rg.name
   allocation_method   = var.environment == "production" ? "Static" : "Dynamic"
+  domain_name_label   = var.domain_name_label
 }
 
 resource "azurerm_network_security_group" "web_server_nsg" {
@@ -76,20 +72,18 @@ resource "azurerm_network_security_group" "web_server_nsg" {
   resource_group_name = azurerm_resource_group.web_server_rg.name
 }
 
-resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
+resource "azurerm_network_security_rule" "web_server_nsg_rule_http" {
   access                      = "Allow"
   direction                   = "Inbound"
-  name                        = "RDP Inbound"
+  name                        = "HTTP Inbound"
   network_security_group_name = azurerm_network_security_group.web_server_nsg.name
   priority                    = 100
   protocol                    = "TCP"
   resource_group_name         = azurerm_resource_group.web_server_rg.name
   source_port_range           = "*"
-  destination_port_range      = "3389"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  # Wll REMOVE the given resource if environment is equal to "production"
-  count                       = var.environment == "production" ? 0 : 1
 }
 
 resource "azurerm_virtual_machine_scale_set" "web_server" {
@@ -134,6 +128,44 @@ resource "azurerm_virtual_machine_scale_set" "web_server" {
       name = local.web_server_name
       primary = true
       subnet_id = azurerm_subnet.web_server_subnet[0].id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id]
     }
   }
+}
+
+resource "azurerm_lb" "web_server_lb" {
+  name                  = "${local.web_server_name}-lb"
+  location              = var.web_server_location
+  resource_group_name   = azurerm_resource_group.web_server_rg.name
+
+  frontend_ip_configuration {
+    name                 = "${local.web_server_name}-lb-frontend-ip"
+    public_ip_address_id = azurerm_public_ip.web_server_lb_public_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "web_server_lb_backend_pool" {
+  name                  = "${local.web_server_name}-lb-backend-pool"
+  resource_group_name   = azurerm_resource_group.web_server_rg.name
+  loadbalancer_id       = azurerm_lb.web_server_lb.id
+}
+
+resource "azurerm_lb_probe" "web_server_lb_http_probe" {
+  name                  = "${local.web_server_name}-lb-http-probe"
+  resource_group_name   = azurerm_resource_group.web_server_rg.name
+  loadbalancer_id       = azurerm_lb.web_server_lb.id
+  port = 80
+  protocol = "TCP"
+}
+
+resource "azurerm_lb_rule" "web_server_lb_http_rule" {
+  name                  = "${local.web_server_name}-lb-http-rule"
+  resource_group_name   = azurerm_resource_group.web_server_rg.name
+  loadbalancer_id       = azurerm_lb.web_server_lb.id
+  protocol = "TCP"
+  frontend_port = 80
+  backend_port = 80
+  frontend_ip_configuration_name = "${local.web_server_name}-lb-frontend-ip"
+  probe_id = azurerm_lb_probe.web_server_lb_http_probe.id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -9,3 +9,4 @@ environment               = "production"
 web_server_count          = 2
 web_server_subnets        = ["1.0.1.0/24", "1.0.2.0/24"]
 terraform_script_version  = "1.0.0"
+domain_name_label         = "balivo-learn-tf-web"
